22edc489cdd68c2cb5aa025e5b0fffdc7f22ee49
Christoph Erdmann <cerdmann@matero.de>
2024-06-26 10:38:10 +0200

    init

---
+++ Classes/ConfigurationModifier.php
@@ -0,0 +1,151 @@
+<?php
+declare(strict_types=1);
+
+namespace Smic\DynamicRoutingPages;
+
+use Doctrine\DBAL\ArrayParameterType;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class ConfigurationModifier
+{
+    protected static $cache = [];
+
+    public static function modifyConfiguration(array $configuration): array
+    {
+        foreach ($configuration as $siteKey => $siteConfiguration) {
+            $configuration[$siteKey] = self::modifySiteConfiguration($siteConfiguration);
+        }
+        return $configuration;
+    }
+
+    protected static function modifySiteConfiguration(array $siteConfiguration): array
+    {
+        if (!isset($siteConfiguration['routeEnhancers'])) {
+            return $siteConfiguration;
+        }
+
+        foreach ($siteConfiguration['routeEnhancers'] as $key => $enhancerConfiguration) {
+            if (!isset($enhancerConfiguration['dynamicPages'])) {
+                continue;
+            }
+            $enhancerConfiguration['limitToPages'] = self::findDynamicPages($enhancerConfiguration['dynamicPages']);
+            $siteConfiguration['routeEnhancers'][$key] = $enhancerConfiguration;
+        }
+        return $siteConfiguration;
+    }
+
+    protected static function findDynamicPages(array $dynamicPagesConfiguration): array
+    {
+        $pageUids = [];
+        if (isset($dynamicPagesConfiguration['withPlugin'])) {
+            $withPlugins = is_array($dynamicPagesConfiguration['withPlugin']) ? $dynamicPagesConfiguration['withPlugin'] : [$dynamicPagesConfiguration['withPlugin']];
+            $withPluginsCacheKey = sha1(json_encode($withPlugins));
+            self::$cache[$withPluginsCacheKey] = self::$cache[$withPluginsCacheKey] ?? self::findPagesWithPlugins($withPlugins);
+            array_push($pageUids, ...self::$cache[$withPluginsCacheKey]);
+        }
+        if (isset($dynamicPagesConfiguration['withDoktypes'])) {
+            $withDoktypes = is_array($dynamicPagesConfiguration['withDoktypes']) ? $dynamicPagesConfiguration['withDoktypes'] : [$dynamicPagesConfiguration['withDoktypes']];
+            $withDoktypesCacheKey = sha1(json_encode($withDoktypes));
+            self::$cache[$withDoktypesCacheKey] = self::$cache[$withDoktypesCacheKey] ?? self::findPagesWithDoktypes($withDoktypes);
+            array_push($pageUids, ...self::$cache[$withDoktypesCacheKey]);
+        }
+        if (isset($dynamicPagesConfiguration['containsModule'])) {
+            $containsModules = is_array($dynamicPagesConfiguration['containsModule']) ? $dynamicPagesConfiguration['containsModule'] : [$dynamicPagesConfiguration['containsModule']];
+            $containsModulesCacheKey = sha1(json_encode($containsModules));
+            self::$cache[$containsModulesCacheKey] = self::$cache[$containsModulesCacheKey] ?? self::findPagesContainingModules($containsModules);
+            array_push($pageUids, ...self::$cache[$containsModulesCacheKey]);
+        }
+        if (isset($dynamicPagesConfiguration['withSwitchableControllerAction'])) {
+            $withSwitchableControllerActions = is_array($dynamicPagesConfiguration['withSwitchableControllerAction']) ? $dynamicPagesConfiguration['withSwitchableControllerAction'] : [$dynamicPagesConfiguration['withSwitchableControllerAction']];
+            $withSwitchableControllerActionsCacheKey = sha1(json_encode($withSwitchableControllerActions));
+            self::$cache[$withSwitchableControllerActionsCacheKey] = self::$cache[$withSwitchableControllerActionsCacheKey] ?? self::findPagesWithSwitchableControllerActions($withSwitchableControllerActions);
+            array_push($pageUids, ...self::$cache[$withSwitchableControllerActionsCacheKey]);
+        }
+        if (isset($dynamicPagesConfiguration['withCType'])) {
+            $withCType = is_array($dynamicPagesConfiguration['withCType']) ? $dynamicPagesConfiguration['withCType'] : [$dynamicPagesConfiguration['withCType']];
+            $withCTypeCacheKey = sha1(json_encode($withCType));
+            self::$cache[$withCTypeCacheKey] = self::$cache[$withCTypeCacheKey] ?? self::findPagesWithCType($withCType);
+            array_push($pageUids, ...self::$cache[$withCTypeCacheKey]);
+        }
+        return array_unique($pageUids);
+    }
+
+    protected static function findPagesWithPlugins(array $withPlugins): array
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
+        $contentElementRecords = $queryBuilder
+            ->select('pid')
+            ->from('tt_content')
+            ->where(
+                $queryBuilder->expr()->eq('CType', $queryBuilder->createNamedParameter('list', Connection::PARAM_STR)),
+                $queryBuilder->expr()->in('list_type', $queryBuilder->createNamedParameter($withPlugins, ArrayParameterType::STRING))
+            )
+            ->executeQuery()
+            ->fetchFirstColumn();
+        return $contentElementRecords;
+    }
+
+    protected static function findPagesWithSwitchableControllerActions(array $withSwitchableControllerActions): array
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
+        $constraints = [];
+        foreach ($withSwitchableControllerActions as $withSwitchableControllerAction) {
+            $constraints[] = $queryBuilder->expr()->like('pi_flexform', $queryBuilder->createNamedParameter('%>' . htmlentities($withSwitchableControllerAction) . '<%', \PDO::PARAM_STR));
+        }
+        $contentElementRecords = $queryBuilder
+            ->select('pid')
+            ->from('tt_content')
+            ->where($queryBuilder->expr()->or(...$constraints))
+            ->executeQuery()
+            ->fetchFirstColumn();
+        return $contentElementRecords;
+    }
+
+    protected static function findPagesContainingModules(array $modules): array
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
+        $pageRecords = $queryBuilder
+            ->select('uid')
+            ->from('pages')
+            ->where(
+                $queryBuilder->expr()->in('module', $queryBuilder->createNamedParameter($modules, ArrayParameterType::STRING))
+            )
+            ->executeQuery()
+            ->fetchFirstColumn();
+        return $pageRecords;
+    }
+
+    protected static function findPagesWithDoktypes(array $doktypes): array
+    {
+        $doktypes = array_map('intval', $doktypes);
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('pages');
+        $pageRecords = $queryBuilder
+            ->select('uid')
+            ->from('pages')
+            ->where(
+                $queryBuilder->expr()->in(
+                    'doktype',
+                    $queryBuilder->createNamedParameter($doktypes, ArrayParameterType::INTEGER)
+                )
+            )
+            ->executeQuery()
+            ->fetchFirstColumn();
+        return $pageRecords;
+    }
+
+    protected static function findPagesWithCType(array $withCType): array
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable('tt_content');
+        $contentElementRecords = $queryBuilder
+            ->select('pid')
+            ->from('tt_content')
+            ->where(
+                $queryBuilder->expr()->in('CType', $queryBuilder->createNamedParameter($withCType, ArrayParameterType::STRING))
+            )
+            ->executeQuery()
+            ->fetchFirstColumn();
+        return $contentElementRecords;
+    }
+}
---
+++ README.md
@@ -0,0 +1,75 @@
+# Dynamic Routing Pages
+
+Instead of hardcoding the `limitToPages` configuration for your route enhancers this package can automatically detect
+the necessary pages for you and generate the configuration on the fly.
+
+## Problem
+
+Imagine the following typical routing configuration for the news plugin.
+
+````
+routeEnhancers:
+  NewsPages:
+    type: Extbase
+    # add every page-ID that contains a News Plugin
+    limitToPages:
+      - 23
+      - 42
+      - 123
+      - 242
+    extension: News
+    plugin: Pi1
+    routes:
+      - { routePath: '/{myNewsTitle}', _controller: 'News::detail', _arguments: {'myNewsTitle': 'news'} }
+      - { routePath: '/{myPagination}', _controller: 'News::list', _arguments: {'myPagination': '@widget_0/currentPage'} }
+      - { routePath: '/{year}/{month}', _controller: 'News::list', _arguments: {'year' : 'overwriteDemand/year', 'month' : 'overwriteDemand/month'} }
+    defaultController: 'News::list'
+    # ...
+````
+
+Hardcoding the page ids for your plugin routes has the major drawback that you have to adapt the configuration as soon as someone creates a new page with a plugin (which might happen in a CMS).
+With the route enhancers configuration not available in the Site module this means you have to ship an updated configuration file every time an editor creates a plugin page.
+
+## Solution
+
+````
+routeEnhancers:
+  NewsPages:
+    type: Extbase
+    dynamicPages:
+        withPlugin: news_pi1
+    extension: News
+    plugin: Pi1
+    routes:
+      - { routePath: '/{myNewsTitle}', _controller: 'News::detail', _arguments: {'myNewsTitle': 'news'} }
+      - { routePath: '/{myPagination}', _controller: 'News::list', _arguments: {'myPagination': '@widget_0/currentPage'} }
+      - { routePath: '/{year}/{month}', _controller: 'News::list', _arguments: {'year' : 'overwriteDemand/year', 'month' : 'overwriteDemand/month'} }
+    defaultController: 'News::list'
+    # ...
+````
+
+Notice the `dynamicPages` configuration. This package will populate the `limitToPages` with matching pages.
+
+## Reference
+
+`dynamicPages` has three possible properties.
+
+### `withPlugin`
+
+Can be a string or an array of `tt_content.list_type` values. Will find all pages that contain at least one of the given plugins.
+
+### `containsModule`
+
+Can be a string or an array of `pages.module` values. Will find all pages that have "Contains Plugin" set to one of the given values.
+
+### `withSwitchableControllerAction`
+
+Can be a string or an array of `switchableControllerActions` values. Will find all pages that contain plugins with the given action configured.
+
+### `withCType`
+
+Can be a string or an array of `withCType` values. Will find all pages that contain content element with given CType.
+
+### `withDoktypes`
+
+Can be an integer or an array of `withDoktypes` values. Will find all pages with given doktype.
